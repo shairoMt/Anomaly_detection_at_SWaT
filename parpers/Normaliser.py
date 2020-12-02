8f2dffc1dd04e444a58b3c40ef8d55ba6e957b39
AdnanMatini <72418525+AdnanMatini@users.noreply.github.com>
2020-12-02 03:54:27 +0100

    Add files via upload

---
+++ parpers/Normaliser.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+
+class Normaliser(object):
+
+    def __init__(self, filePath, batchSize):
+        self.filePath = filePath
+        self.batchSize = batchSize
+        self.min_val, self.max_val, self.mean_val, self.length = self.findNormaliser()
+
+    def findNormaliserOfBatch(self,  batch):
+        length = np.shape(batch)[0]
+        min_val = np.min(batch, axis=0)
+        max_val = np.max(batch, axis=0)
+        mean_val = np.mean(batch, axis=0)
+        return length,min_val,max_val,mean_val
+
+    def readAndFilterBatchFromCsv(self, start):
+        batch = pd.read_csv(self.filePath,
+                           sep=",",
+                           decimal=".",
+                           skiprows=start+1,
+                           nrows=self.batchSize
+        )
+        batch = pd.DataFrame.to_numpy(batch)[:, 2:7]
+        return batch
+
+    def findNormaliser(self):
+        start = 0
+        i = 0
+        output = self.readAndFilterBatchFromCsv(start)
+        if(output.size == 0):
+            return -1
+        length, min_val, max_val, mean_val = self.findNormaliserOfBatch(output)
+        while(True):
+            start = start + length
+            output = self.readAndFilterBatchFromCsv(start)
+            if(output.size == 0):
+                break
+            length, min_val_, max_val_, mean_val_ = self.findNormaliserOfBatch(output)
+            i = i+1
+            for j in range(len(min_val)):
+                if(min_val[j] > min_val_[j]):
+                    min_val[j] = min_val_[j]
+                if (max_val[j] < max_val_[j]):
+                    max_val[j] = max_val_[j]
+                mean_val[j] = (mean_val[j]*i*self.batchSize + mean_val_[j]*length)/(i * self.batchSize + length)
+        length = length + self.batchSize * i
+        return min_val, max_val, mean_val, length
+
+    def normaliseBatch(self, batch):
+        for i in range(len(self.mean_val)):
+            batch[:,i] = batch[:,i] - self.mean_val[i]
+            batch[:,i] = batch[:,i] / (self.max_val[i] - self.min_val[i])
+        return batch
+
+
---
+++ parpers/Normaliser_Test.py
@@ -0,0 +1,68 @@
+import numpy as np
+import Normaliser
+
+
+
+def test_readAndFilterBatchFromCsv():
+    n = Normaliser.Normaliser('C:\\Users\\kaspe\\Desktop\\Anomaly_detection_at_SWaT\\chunk.csv', 10)
+    x = n.readAndFilterBatchFromCsv(0)
+    Erwartet = np.array([0 ,124.3135, 1, 1, 2 ])
+    np.testing.assert_array_almost_equal(x[0], Erwartet)
+    n = Normaliser.Normaliser('C:\\Users\\kaspe\\Desktop\\Anomaly_detection_at_SWaT\\chunk.csv', 10)
+    x = n.readAndFilterBatchFromCsv(998)
+    Erwartet = np.array([ 2.501041, 253.8476, 2, 2, 1])
+    np.testing.assert_array_almost_equal(x[0], Erwartet)
+
+def test_equality_findNormaliserOfBatch():
+    n = Normaliser.Normaliser('C:\\Users\\kaspe\\Desktop\\Anomaly_detection_at_SWaT\\chunk.csv', 3)
+    output = n.readAndFilterBatchFromCsv(996)
+    length,min_val,max_val,mean_val = n.findNormaliserOfBatch(output)
+    min_val = np.array(min_val)
+    max_val = np.array(max_val)
+    mean_val = np.array(mean_val)
+    np.testing.assert_equal(length,3)
+    np.testing.assert_array_almost_equal(min_val,[2.436025,253.4551,2,2,1])
+    np.testing.assert_array_almost_equal(max_val,[2.501041,253.8476,2,2,1])
+    np.testing.assert_array_almost_equal(mean_val,[2.4648496667,253.58593333,2,2,1])
+
+def test_not_equality_findNormaliserOfBatch():
+    n = Normaliser.Normaliser('C:\\Users\\kaspe\\Desktop\\Anomaly_detection_at_SWaT\\chunk.csv', 3)
+    output = n.readAndFilterBatchFromCsv(0)
+    length,min_val,max_val,mean_val = n.findNormaliserOfBatch(output)
+    min_val = np.array(min_val)
+    max_val = np.array(max_val)
+    mean_val = np.array(mean_val)
+    np.testing.assert_equal(length,3)
+    np.testing.assert_raises(AssertionError, np.testing.assert_array_almost_equal, min_val, [2.436025,253.4551,2,2,1])
+    np.testing.assert_raises(AssertionError, np.testing.assert_array_almost_equal, max_val, [2.501041,253.8476,2,2,1])
+    np.testing.assert_raises(AssertionError, np.testing.assert_array_almost_equal, mean_val, [2.4648496667,253.58593333,2,2,1])
+
+def test_findNormaliser():
+    n = Normaliser.Normaliser('C:\\Users\\kaspe\\Desktop\\Anomaly_detection_at_SWaT\\chunk.csv', 1000)
+    min_val = n.min_val
+    max_val = n.max_val
+    mean_val = n.mean_val
+
+    output = n.readAndFilterBatchFromCsv(0)
+    min_val_ = np.min(output, axis=0)
+    max_val_ = np.max(output, axis=0)
+    mean_val_ = np.mean(output, axis=0)
+    np.testing.assert_array_almost_equal(min_val, min_val_)
+    np.testing.assert_array_almost_equal(max_val,max_val_)
+    np.testing.assert_array_almost_equal(mean_val,mean_val_)
+
+def test_normaliseBatch():
+    n = Normaliser.Normaliser('C:\\Users\\kaspe\\Desktop\\Anomaly_detection_at_SWaT\\chunk.csv', 3 )
+    batch = n.readAndFilterBatchFromCsv(0)
+    normalised_batch = n.normaliseBatch(batch)
+    np.testing.assert_array_equal(normalised_batch[0],[-0.3705133275464534, -0.22634999748483334, -0.19019019019019023,
+  -0.12512512512512508, 0.9989989989989989])
+
+
+if __name__ == "__main__":
+    test_readAndFilterBatchFromCsv()
+    test_equality_findNormaliserOfBatch()
+    test_not_equality_findNormaliserOfBatch()
+    test_findNormaliser()
+    test_normaliseBatch()
+
